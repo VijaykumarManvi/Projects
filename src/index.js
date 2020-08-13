4f99721cb86bbc0413df4366e1fe5701efe72393
VijaykumarManvi <69640001+VijaykumarManvi@users.noreply.github.com>
2020-08-14 01:58:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { fetchMissionsData } from "./redux/actions";
 
-
 store.dispatch(fetchMissionsData());
 ReactDOM.hydrate(
   <Provider store={store}>
